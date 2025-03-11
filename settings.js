c57c5a05ae1869e39ea2fd897de5eaefc07b337c
MusarrafMH <musarrafhimasha@gmail.com>
2025-03-11 09:13:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'MALVIN-XD~wooXCZpY#G8ZXkKrUg8dV7SWSfiI-3IplktxcDI9PQ6FmWBz3Tts' 
 //Enter your Jinwoo session id here; must start with MALVIN-XD~
 
 //--------------------[ BOT NAME ]----------------------//
